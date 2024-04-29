1ed1a7844629cb373950c2cdcabaab94d4842b41
bigzhu <vermiliondun@gmail.com>
2024-04-29 23:32:55 +0800

    update

--- Solominer_v2.py
+++ Solominer_v2.py
@@ -7,13 +7,14 @@ import socket
 import time
 from threading import Thread
 from colorthon import Colors as Fore
-import sys, logging
+import sys
+import logging
 
 # Define your Bitcoin address
 address = "1MMDRZAcM6dzmdMUSV8pDdAPDFpwzve9Fc"
 # Initialize the current block height
 cHeight = 0
-soloxminer = '''
+soloxminer = """
                             ███████╗ ██████╗ ██╗      ██████╗
                             ██╔════╝██╔═══██╗██║     ██╔═══██╗
                             ███████╗██║   ██║██║     ██║   ██║
@@ -27,9 +28,9 @@ soloxminer = '''
                             ██║╚██╔╝██║██║██║╚██╗██║██╔══╝  ██╔══██╗
                             ██║ ╚═╝ ██║██║██║ ╚████║███████╗██║  ██║
                             ╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝
-'''
+"""
 
-mmdrza = '''
+mmdrza = """
                    |======================================================|
                    |=========== ╔╦╗╔╦╗╔╦╗╦═╗╔═╗╔═╗ ╔═╗╔═╗╔╦╗  ============|
                    |=========== ║║║║║║ ║║╠╦╝╔═╝╠═╣ ║  ║ ║║║║  ============|
@@ -39,7 +40,7 @@ mmdrza = '''
                    |- GiTHUB  ---------------------- Github.Com/PyMmdrza -|
                    |- MEDIUM  -------------- PythonWithMmdrza.Medium.Com -|
                    |======================================================|
-'''
+"""
 
 
 def delay_print(s):
@@ -52,26 +53,31 @@ def delay_print(s):
 print(Fore.RED, soloxminer, Fore.RESET)
 print(Fore.YELLOW, mmdrza, Fore.RESET)
 cHeight = 0
-inpAdd = input(
-    f'{Fore.MAGENTA}[*]{Fore.RESET}{Fore.WHITE} INSERT HERE YOUR ADDRESS BITCOIN WALLET For Withdrawal{Fore.RESET} : ')
-address = str(inpAdd)
-print(f'\n{Fore.GREY}Bitcoin Wallet Address{Fore.RESET} ===>> {Fore.MAGENTA}{address}{Fore.RESET}')
+# inpAdd = input(
+# f'{Fore.MAGENTA}[*]{Fore.RESET}{Fore.WHITE} INSERT HERE YOUR ADDRESS BITCOIN WALLET For Withdrawal{Fore.RESET} : ')
+# address = str(inpAdd)
+address = "19mbLwmV1bKnHjfpxjXKThCisLJcbV7NP3"
+print(
+    f"\n{Fore.GREY}Bitcoin Wallet Address{Fore.RESET} ===>> {Fore.MAGENTA}{address}{Fore.RESET}"
+)
 print(f"{Fore.GREY}{'-' * 66}{Fore.RESET}")
-delay_print(' Your Bitcoin Wallet Address Added For Mining Now ...')
+delay_print(" Your Bitcoin Wallet Address Added For Mining Now ...")
 print(f"\n{Fore.GREY}{'-' * 66}{Fore.RESET}")
 
 time.sleep(3)
 
 
 def logg(msg):
-    logging.basicConfig(level=logging.INFO, filename="miner.log", format='%(asctime)s %(message)s')  # include timestamp
+    logging.basicConfig(
+        level=logging.INFO, filename="miner.log", format="%(asctime)s %(message)s"
+    )  # include timestamp
     logging.info(msg)
 
 
 # Function to get the current network block height
 def get_current_block_height():
-    r = requests.get('https://blockchain.info/latestblock')
-    return int(r.json()['height'])
+    r = requests.get("https://blockchain.info/latestblock")
+    return int(r.json()["height"])
 
 
 # Function for the mining process
@@ -80,89 +86,174 @@ def BitcoinMiner(restart=False):
 
     if restart:
         time.sleep(2)
-        logg('[*] Bitcoin Miner Restarted')
+        logg("[*] Bitcoin Miner Restarted")
     else:
-        logg('[*] Bitcoin Miner Started')
-        print('[*] Bitcoin Miner Started')
+        logg("[*] Bitcoin Miner Started")
+        print("[*] Bitcoin Miner Started")
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(('solo.ckpool.org', 3333))
+    sock.connect(("solo.ckpool.org", 3333))
 
     sock.sendall(b'{"id": 1, "method": "mining.subscribe", "params": []}\n')
 
-    lines = sock.recv(1024).decode().split('\n')
+    lines = sock.recv(1024).decode().split("\n")
 
     response = json.loads(lines[0])
-    sub_details, extranonce1, extranonce2_size = response['result']
+    sub_details, extranonce1, extranonce2_size = response["result"]
 
-    sock.sendall(b'{"params": ["' + address.encode() + b'", "password"], "id": 2, "method": "mining.authorize"}\n')
+    sock.sendall(
+        b'{"params": ["'
+        + address.encode()
+        + b'", "password"], "id": 2, "method": "mining.authorize"}\n'
+    )
 
-    response = b''
-    while response.count(b'\n') < 4 and not (b'mining.notify' in response): response += sock.recv(1024)
+    response = b""
+    while response.count(b"\n") < 4 and not (b"mining.notify" in response):
+        response += sock.recv(1024)
 
-    responses = [json.loads(res) for res in response.decode().split('\n') if
-                 len(res.strip()) > 0 and 'mining.notify' in res]
-    job_id, prevhash, coinb1, coinb2, merkle_branch, version, nbits, ntime, clean_jobs = responses[0]['params']
-    target = (nbits[2:] + '00' * (int(nbits[:2], 16) - 3)).zfill(64)
-    extranonce2 = hex(random.randint(0, 2 ** 32 - 1))[2:].zfill(2 * extranonce2_size)  # create random
+    responses = [
+        json.loads(res)
+        for res in response.decode().split("\n")
+        if len(res.strip()) > 0 and "mining.notify" in res
+    ]
+    (
+        job_id,
+        prevhash,
+        coinb1,
+        coinb2,
+        merkle_branch,
+        version,
+        nbits,
+        ntime,
+        clean_jobs,
+    ) = responses[0]["params"]
+    target = (nbits[2:] + "00" * (int(nbits[:2], 16) - 3)).zfill(64)
+    extranonce2 = hex(random.randint(0, 2**32 - 1))[2:].zfill(
+        2 * extranonce2_size
+    )  # create random
 
     coinbase = coinb1 + extranonce1 + extranonce2 + coinb2
-    coinbase_hash_bin = hashlib.sha256(hashlib.sha256(binascii.unhexlify(coinbase)).digest()).digest()
+    coinbase_hash_bin = hashlib.sha256(
+        hashlib.sha256(binascii.unhexlify(coinbase)).digest()
+    ).digest()
 
     merkle_root = coinbase_hash_bin
     for h in merkle_branch:
-        merkle_root = hashlib.sha256(hashlib.sha256(merkle_root + binascii.unhexlify(h)).digest()).digest()
+        merkle_root = hashlib.sha256(
+            hashlib.sha256(merkle_root + binascii.unhexlify(h)).digest()
+        ).digest()
 
     merkle_root = binascii.hexlify(merkle_root).decode()
 
-    merkle_root = ''.join([merkle_root[i] + merkle_root[i + 1] for i in range(0, len(merkle_root), 2)][::-1])
+    merkle_root = "".join(
+        [merkle_root[i] + merkle_root[i + 1] for i in range(0, len(merkle_root), 2)][
+            ::-1
+        ]
+    )
 
     work_on = get_current_block_height()
-    print(Fore.GREEN, 'Working on current Network height', Fore.WHITE, work_on)
-    print(Fore.YELLOW, 'Current TARGET =', Fore.RED, target)
+    print(Fore.GREEN, "Working on current Network height", Fore.WHITE, work_on)
+    print(Fore.YELLOW, "Current TARGET =", Fore.RED, target)
     z = 0
     while True:
         if cHeight > work_on:
-            logg('[*] Restarting Miner')
+            logg("[*] Restarting Miner")
             BitcoinMiner(restart=True)
             break
 
-        nonce = hex(random.randint(0, 2 ** 32 - 1))[2:].zfill(8)  # nnonve   #hex(int(nonce,16)+1)[2:]
-        blockheader = version + prevhash + merkle_root + nbits + ntime + nonce + \
-                      '000000800000000000000000000000000000000000000000000000000000000000000000000000000000000080020000'
-        hash = hashlib.sha256(hashlib.sha256(binascii.unhexlify(blockheader)).digest()).digest()
+        nonce = hex(random.randint(0, 2**32 - 1))[2:].zfill(
+            8
+        )  # nnonve   #hex(int(nonce,16)+1)[2:]
+        blockheader = (
+            version
+            + prevhash
+            + merkle_root
+            + nbits
+            + ntime
+            + nonce
+            + "000000800000000000000000000000000000000000000000000000000000000000000000000000000000000080020000"
+        )
+        hash = hashlib.sha256(
+            hashlib.sha256(binascii.unhexlify(blockheader)).digest()
+        ).digest()
         hash = binascii.hexlify(hash).decode()
 
-        if hash.startswith('000000000000000000000'): logg('hash: {}'.format(hash))
-        print(Fore.GREEN, str(z), ' HASH :', Fore.YELLOW, ' 000000000000000000000{}'.format(hash), end='\r')
+        if hash.startswith("000000000000000000000"):
+            logg("hash: {}".format(hash))
+        print(
+            Fore.GREEN,
+            str(z),
+            " HASH :",
+            Fore.YELLOW,
+            " 000000000000000000000{}".format(hash),
+            end="\r",
+        )
         z += 1
-        if hash.startswith('000000000000000000'): logg('hash: {}'.format(hash))
-        z += 1
-
-        print(Fore.YELLOW, str(z), 'HASH :', Fore.RED, ' 000000000000000000{}'.format(hash), end='\r')
-        z += 1
-
-        if hash.startswith('000000000000000'): logg('hash: {}'.format(hash))
-        print(Fore.BLUE, str(z), 'HASH :', Fore.GREEN, ' 000000000000000{}'.format(hash), end='\r')
-        z += 1
-
-        if hash.startswith('000000000000'): logg('hash: {}'.format(hash))
-        print(Fore.MAGENTA, str(z), 'HASH :', Fore.YELLOW, ' 000000000000{}'.format(hash), end='\r')
+        if hash.startswith("000000000000000000"):
+            logg("hash: {}".format(hash))
         z += 1
 
-        if hash.startswith('0000000'): logg('hash: {}'.format(hash))
-        print(Fore.CYAN, str(z), 'HASH :', Fore.YELLOW, '0000000{}'.format(hash), end='\r')
+        print(
+            Fore.YELLOW,
+            str(z),
+            "HASH :",
+            Fore.RED,
+            " 000000000000000000{}".format(hash),
+            end="\r",
+        )
+        z += 1
+
+        if hash.startswith("000000000000000"):
+            logg("hash: {}".format(hash))
+        print(
+            Fore.BLUE,
+            str(z),
+            "HASH :",
+            Fore.GREEN,
+            " 000000000000000{}".format(hash),
+            end="\r",
+        )
+        z += 1
+
+        if hash.startswith("000000000000"):
+            logg("hash: {}".format(hash))
+        print(
+            Fore.MAGENTA,
+            str(z),
+            "HASH :",
+            Fore.YELLOW,
+            " 000000000000{}".format(hash),
+            end="\r",
+        )
+        z += 1
+
+        if hash.startswith("0000000"):
+            logg("hash: {}".format(hash))
+        print(
+            Fore.CYAN, str(z), "HASH :", Fore.YELLOW, "0000000{}".format(hash), end="\r"
+        )
         z += 1
 
         if hash < target:
-            print('[*] New block mined')
-            logg('[*] success!!')
+            print("[*] New block mined")
+            logg("[*] success!!")
             logg(blockheader)
-            logg('hash: {}'.format(hash))
+            logg("hash: {}".format(hash))
 
             payload = bytes(
-                '{"params": ["' + address + '", "' + job_id + '", "' + extranonce2 \
-                + '", "' + ntime + '", "' + nonce + '"], "id": 1, "method": "mining.submit"}\n', 'utf-8')
+                '{"params": ["'
+                + address
+                + '", "'
+                + job_id
+                + '", "'
+                + extranonce2
+                + '", "'
+                + ntime
+                + '", "'
+                + nonce
+                + '"], "id": 1, "method": "mining.submit"}\n',
+                "utf-8",
+            )
             sock.sendall(payload)
             logg(payload)
             ret = sock.recv(1024)
@@ -179,17 +270,17 @@ def newBlockListener():
         network_height = get_current_block_height()
 
         if network_height > cHeight:
-            logg('[*] Network has new height %d ' % network_height)
-            logg('[*] Our local is %d' % cHeight)
+            logg("[*] Network has new height %d " % network_height)
+            logg("[*] Our local is %d" % cHeight)
             cHeight = network_height
-            logg('[*] Our new local after update is %d' % cHeight)
+            logg("[*] Our new local after update is %d" % cHeight)
 
         # respect Api
         time.sleep(40)
 
 
 # Main function to start the miner and block listener
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Start the block listener and miner threads
     Thread(target=newBlockListener).start()
     time.sleep(2)
